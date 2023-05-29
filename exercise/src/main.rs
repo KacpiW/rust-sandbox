68de050f35282a39398c642d73e09f49f78fd128
KacpiW <kacper.wozni@gmail.com>
2023-05-29 21:04:15 +0200

    master: Gitignore and new main

--- exercise/src/main.rs
+++ exercise/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x: i8 = 7;
+    let y: &str = "Hello";
+    print!("{y} -> ");
+    print!("{}", x);
+    while x != 1 {
+        if x % 2 == 0 {
+            x = x / 2;
+        } else {
+            x = 3 * x + 1;
+        }
+        print!(" -> {x}");
+    }
+    println!();
 }
